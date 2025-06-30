8bc2031af14e5f5c8ea3832e846c7c306273fde1
David Picard <david.picard@clermont.in2p3.fr>
2025-06-30 13:45:39 +0200

    fixup! Add Enclustra Mercury+ SA2 module

--- arch/arm/boards/enclustra-sa2/lowlevel.c
+++ arch/arm/boards/enclustra-sa2/lowlevel.c
@@ -7,7 +7,7 @@
 #include "sequencer_auto_ac_init.c"
 #include "iocsr_config_cyclone5.c"
 
-#include <mach/lowlevel.h>
+#include <mach/socfpga/lowlevel.h>
 
 SOCFPGA_C5_ENTRY(start_socfpga_sa2, socfpga_cyclone5_mercury_sa2, SZ_1G);
 SOCFPGA_C5_XLOAD_ENTRY(start_socfpga_sa2_xload, SZ_1G);
