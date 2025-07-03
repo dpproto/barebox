1c8ed2fc0fa67819d8226a0fc7fe474975a1f068
David Picard <david.picard@clermont.in2p3.fr>
2025-07-03 09:54:52 +0200

    fixup! Add Enclustra Mercury+ SA2 module

--- arch/arm/boards/enclustra-sa2/board.c
+++ arch/arm/boards/enclustra-sa2/board.c
@@ -25,7 +25,7 @@ static int phy_fixup(struct phy_device *dev)
 
 static int socfpga_init(void)
 {
-	if (!of_machine_is_compatible("terasic,de0-nano-soc"))
+	if (!of_machine_is_compatible("altr,socfpga-cyclone5"))
 		return 0;
 
 	if (IS_ENABLED(CONFIG_PHYLIB))
