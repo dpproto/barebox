683f165a97f92a2b5db36bd046a63af3e198d007
David Picard <david.picard@clermont.in2p3.fr>
2025-07-09 16:02:34 +0200

    Debug msg: board.c

--- arch/arm/boards/enclustra-sa2/board.c
+++ arch/arm/boards/enclustra-sa2/board.c
@@ -11,11 +11,14 @@
 #include <fs.h>
 #include <mach/socfpga/cyclone5-regs.h>
 
+#include <debug_ll.h>
+
 /*
  * Ethernet PHY: Microchip/Micrel KSZ9031RNX
  */
 static int phy_fixup(struct phy_device *dev)
 {
+	puts_ll("board.c: phy_fixup() >> start\n");
 	/*
 	 * min rx data delay, max rx/tx clock delay,
 	 * min rx/tx control delay
@@ -23,17 +26,22 @@ static int phy_fixup(struct phy_device *dev)
 	phy_write_mmd(dev, MDIO_MMD_WIS, 4, 0);
 	phy_write_mmd(dev, MDIO_MMD_WIS, 5, 0);
 	phy_write_mmd(dev, MDIO_MMD_WIS, 8, 0x003ff);
+	puts_ll("board.c: phy_fixup() >> end\n");
 	return 0;
 }
 
 static int socfpga_init(void)
 {
+	puts_ll("board.c: socfpga_init() >> start\n");
 	if (!of_machine_is_compatible("altr,socfpga-cyclone5"))
 		return 0;
+	puts_ll("board.c: socfpga_init() >> compat OK\n");
+
 
 	if (IS_ENABLED(CONFIG_PHYLIB))
 		phy_register_fixup_for_uid(PHY_ID_KSZ9031, MICREL_PHY_ID_MASK, phy_fixup);
 
+	puts_ll("board.c: socfpga_init() >> end\n");
 	return 0;
 }
 console_initcall(socfpga_init);
